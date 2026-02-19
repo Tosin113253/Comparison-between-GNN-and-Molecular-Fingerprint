9b8339ac74a79de54e1c15f40d772803ac2b1b7e
Tosin113253 <105084603+Tosin113253@users.noreply.github.com>
2026-02-19 15:28:13 -0500

    Update training.py

--- GNN_photodegradation/training.py
+++ GNN_photodegradation/training.py
@@ -21,7 +21,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.linear_model import Ridge
 from sklearn.ensemble import RandomForestRegressor
-from GNN_photodegradation.models.gcn_model import StrongGCN as GNNModel
+from GNN_photodegradation.models.gcn_model import StrongGCN
+
 
 import shap
 import matplotlib.pyplot as plt
